d49678102c3629a7dfdae76dfa5e8c2eecf7e243
GizIOn <34921969+GizIOn@users.noreply.github.com>
2022-10-29 17:16:24 +0500

    dev Create base structure

--- Hackathon2022-backend/src/main/kotlin/com/example/Hackathon2022Spring/Hackathon2022Application.kt
+++ Hackathon2022-backend/src/main/kotlin/com/example/Hackathon2022Spring/Hackathon2022Application.kt
@@ -1,9 +1,13 @@
 package com.example.Hackathon2022Spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 class Hackathon2022Application
 
 fun main(args: Array<String>) {
