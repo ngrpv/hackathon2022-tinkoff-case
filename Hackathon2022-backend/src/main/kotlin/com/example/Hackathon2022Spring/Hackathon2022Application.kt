411b91f28498e27aa8755c243271866c87f07f7e
Nazar Garipov <n.garipov@skbkontur.ru>
2022-10-29 13:34:45 +0500

    Add back

---
+++ Hackathon2022-backend/src/main/kotlin/com/example/Hackathon2022Spring/Hackathon2022Application.kt
@@ -0,0 +1,11 @@
+package com.example.Hackathon2022Spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hackathon2022Application
+
+fun main(args: Array<String>) {
+	runApplication<Hackathon2022Application>(*args)
+}
---
+++ Hackathon2022-backend/src/test/kotlin/com/example/Hackathon2022Spring/Hackathon2022ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Hackathon2022Spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Hackathon2022ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
