1d8e8f16c6572314081f45c442da65d68b413d8e
GizIOn <34921969+GizIOn@users.noreply.github.com>
2022-10-30 11:00:10 +0500

    string return to front

--- Hackathon2022-backend/src/main/kotlin/com/example/Hackathon2022Spring/controllers/RegistrationController.kt
+++ Hackathon2022-backend/src/main/kotlin/com/example/Hackathon2022Spring/controllers/RegistrationController.kt
@@ -1,7 +1,7 @@
 package com.example.Hackathon2022Spring.controllers
 
-import com.example.Hackathon2022Spring.models.enums.LoginResponseType
 import com.example.Hackathon2022Spring.models.RegisterInfo
+import com.example.Hackathon2022Spring.models.enums.LoginResponseType
 import com.example.Hackathon2022Spring.models.enums.LogoutResponseType
 import com.example.Hackathon2022Spring.models.enums.RegisterResponseType
 import com.example.Hackathon2022Spring.repositories.RegisterInfoRepository
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
 import javax.inject.Inject
 
 @Controller
@@ -20,26 +21,26 @@ class RegistrationController @Inject constructor(
 
     @GetMapping("/register")
     @ResponseBody
-    fun register(@RequestBody registerInfo: RegisterInfo): RegisterResponseType {
+    fun register(@RequestBody registerInfo: RegisterInfo): String {
         val user = registerInfoRepository.getByEmail(registerInfo.email);
-        return if (user.equals(null)) {
-            registerInfoRepository.save(registerInfo)
-            RegisterResponseType.Successful;
+        val registerResponseType = registerResponseType(user, registerInfo)
+        return if (registerResponseType == RegisterResponseType.Successful) {
+            registerInfo.email
         } else {
-            RegisterResponseType.EmailExists;
+            ""
         }
     }
 
     @GetMapping("/login")
     @ResponseBody
-    fun login(@RequestBody registerInfo: RegisterInfo): LoginResponseType {
+    fun login(@RequestBody registerInfo: RegisterInfo): String {
         val user = registerInfoRepository.getByEmail(registerInfo.email)
-        return if (user.equals(null)) {
-            LoginResponseType.UnknownEmail
+        val loginResponseType = loginResponseType(user)
+
+        return if (loginResponseType == LoginResponseType.Successful) {
+            registerInfo.email
         } else {
-            val userFromUsers = userRepository.findUserByEmail(user.email)
-            userFromUsers.isActive = true;
-            LoginResponseType.Successful
+            ""
         }
     }
 
@@ -52,4 +53,25 @@ class RegistrationController @Inject constructor(
         return LogoutResponseType.Successful
     }
 
+    private fun registerResponseType(
+        user: RegisterInfo,
+        registerInfo: RegisterInfo
+    ): RegisterResponseType {
+        if (user.equals(null)) {
+            registerInfoRepository.save(registerInfo)
+            return RegisterResponseType.Successful;
+        } else {
+            return RegisterResponseType.EmailExists;
+        }
+    }
+
+    private fun loginResponseType(user: RegisterInfo): LoginResponseType {
+        return if (user.equals(null)) {
+            LoginResponseType.UnknownEmail
+        } else {
+            val userFromUsers = userRepository.findUserByEmail(user.email)
+            userFromUsers.isActive = true;
+            LoginResponseType.Successful
+        }
+    }
 }
